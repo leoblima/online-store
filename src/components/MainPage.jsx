981847c21542def36152a96a2c4de32e438b04f7
Michele Gusmao <michele.gusmao7@gmail.com>
2022-04-26 15:49:01 -0300

    Adiciona requisito 2.

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="" id="" />
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
