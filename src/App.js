27d5416673dc16eafea4294b0cc0a2c55239bdef
Michele Gusmao <michele.gusmao7@gmail.com>
2022-04-26 20:37:39 -0300

    Adiciona requisitos do 1 ao 8.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import MainPage from './components/MainPage';
+import ShoppingCart from './components/ShopingCart';
+import ProductDetails from './components/ProductDetails';
 import './App.css';
 
 class App extends React.Component {
@@ -9,6 +11,12 @@ class App extends React.Component {
       <div>
         <BrowserRouter>
           <Route exact path="/" component={ MainPage } />
+          <Route exact path="/shoppingcart" component={ ShoppingCart } />
+          <Route
+            exact
+            path="/productdetails"
+            render={ (props) => <ProductDetails { ...props } /> }
+          />
         </BrowserRouter>
       </div>
     );
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,15 +1,110 @@
 import React from 'react';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import ProductCard from './ProductCard';
+import CartButton from './CartButton';
 
 class MainPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      inputValue: '',
+      products: [],
+      category: '',
+      cart: [],
+    };
+  }
+
+  componentDidMount() {
+    this.callApiCategories();
+  }
+
+  callApiCategories = async () => {
+    const result = await getCategories();
+    this.setState({
+      categories: result,
+    });
+  }
+
+  handleInput = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      inputValue: value,
+    });
+  }
+
+  searchApi = async () => {
+    const { inputValue, category } = this.state;
+    const result = await getProductsFromCategoryAndQuery(category, inputValue);
+    this.setState({
+      products: result.results,
+    });
+  }
+
+  searchCategory = ({ target }) => {
+    const { id } = target;
+    this.setState(({
+      category: id,
+    }), () => this.searchApi());
+  }
+
+  addToCart = (title, price, thumbnail) => {
+    this.setState((prevState) => ({
+      cart: [...prevState.cart, { title, price, thumbnail }],
+    }));
+  }
+
   render() {
+    const { categories, products, cart } = this.state;
     return (
       <div>
-        <input type="text" name="" id="" />
+        <input
+          data-testid="query-input"
+          type="text"
+          name="inputValue"
+          id=""
+          onChange={ this.handleInput }
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.searchApi }
+        >
+          Pesquisar
+        </button>
+        <CartButton cart={ cart } />
         <p
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        {categories.map((element) => {
+          const { name, id } = element;
+          return (
+            <button
+              data-testid="category"
+              type="button"
+              key={ name }
+              id={ id }
+              onClick={ this.searchCategory }
+            >
+              { name }
+            </button>
+          );
+        })}
+        {products.length > 0 && (
+          products.map((element) => {
+            const { title, price, thumbnail } = element;
+            return (
+              <ProductCard
+                key={ title }
+                title={ title }
+                price={ price }
+                thumbnail={ thumbnail }
+                addToCart={ this.addToCart }
+              />
+            );
+          }))}
       </div>
     );
   }
